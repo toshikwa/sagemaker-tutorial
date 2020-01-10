149db2b275d7fd35f732c0180643f6f605c9af9a
Toshiki Watanabe <kuboy2482@gmail.com>
2020-01-10 15:54:12 +0900

    add text vectorizer and tree(WIP)

---
+++ ris/__init__.py
@@ -0,0 +1,3 @@
+from .utils import convert_text_into_sentences
+from .vectorizer import TextVectorizer
+from .node import Node
---
+++ ris/utils.py
@@ -0,0 +1,31 @@
+import re
+from cleantext import clean
+from nltk.tokenize import sent_tokenize
+
+
+def convert_text_into_sentences(text):
+    assert isinstance(text, str)
+
+    # Normalize a text.
+    text = clean(
+        text,
+        fix_unicode=True,
+        to_ascii=True,
+        lower=True,
+        no_line_breaks=False,
+        no_urls=True,
+        no_emails=True,
+        no_phone_numbers=True,
+        no_numbers=False,
+        no_digits=False,
+        no_currency_symbols=False,
+        no_punct=False)
+
+    # Remove tags.
+    text = re.sub('<[^<]+?>', '', text)
+    # Remove invalid charactors.
+    text = re.sub('[#%\'\(\)\*\+\-\\\/:;<=>@^_`|~\[\]]+', '', text)
+    # Convert a text into sentences.
+    sentences = sent_tokenize(text)
+
+    return sentences
