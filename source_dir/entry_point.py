c98be11a13dbd431cab2e8f5e5d29ef29e128788
Toshiki <kuboy2482@gmail.com>
2020-01-24 16:04:22 +0000

    fix entry_point to use search module

--- source_dir/entry_point.py
+++ source_dir/entry_point.py
@@ -5,6 +5,7 @@ import subprocess
 import json
 import pickle
 import numpy as np
+import pandas as pd
 import torch
 from  torch import nn
 from sentence_transformers import SentenceTransformer
@@ -12,16 +13,17 @@ from search import ProductSearch, convert_text_into_sentences
 
 
 def model_fn(model_dir):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     # Load trained vectorizer.
     with open(os.path.join(model_dir, 'modules.pickle'),'rb') as f:
         modules = pickle.load(f)
     vectorizer = SentenceTransformer(modules=modules).eval()
 
     # Load trained search engine.
-    with open(os.path.join(model_dir, 'product_search.pickle'), 'rb') as f:
-        product_search = pickle.load(f)
+    product_search = ProductSearch(
+        index_path=os.path.join(model_dir, 'product_search.pickle'))
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     return {
         'vectorizer': vectorizer.to(device),
         'product_search': product_search
@@ -44,8 +46,13 @@ def predict_fn(data, model):
         embeddings = np.array(vectorizer.encode(sentences), dtype=np.float32)
 
     # Search.
-    search_results = product_search.search(embeddings)
+    prediction = product_search.search(embeddings)
     prediction = {f'pred{str(i)}': pred for i, pred in enumerate(prediction)}
+    
+    # Convert np.float32 into np.float64 to serialize.
+    for key in prediction.keys():
+        prediction[key]['product_search_score'] =\
+            prediction[key]['product_search_score'].astype(np.float64)
 
     return prediction
 
@@ -67,9 +74,8 @@ def train(args):
 
     # Construct the search engine.
     product_search = ProductSearch(reviews, sentences, embeddings)
-
-    with open(os.path.join(args.model_dir, 'product_search.pickle'), 'wb') as f:
-        pickle.dump(product_search, f)
+    product_search.save(
+        os.path.join(args.model_dir, 'product_search.pickle'))
 
 
 if __name__ == '__main__':
